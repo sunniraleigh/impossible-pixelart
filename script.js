d5f9bf3b3608ce1f8b9d695cd626ea3f90859df9
sunniraleigh <sraleigh@smith.edu>
2023-03-21 13:39:40 -0400

    implement individual painting rotations

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const bunnyReaction = new Image();
 
 // define other vars
 var speed = 10;
-var rotSpeed = 5;
 
 function init(){
   // connect to src images
@@ -50,12 +49,27 @@ function drawGallery(){
   ctx.drawImage(background, 0, 0);
 
   // paintings
-  ctx.drawImage(cow, cow.posX, cow.posY);
-  ctx.drawImage(bear, bear.posX, bear.posY);
-  ctx.drawImage(bunny, bunny.posX, bunny.posY);
+  drawPainting(ctx, cow);
+  drawPainting(ctx, bear);
+  drawPainting(ctx, bunny);
 
   // merlin
   ctx.drawImage(merlin, merlin.posX, merlin.posY);
+}
+
+function drawPainting(ctx, painting){
+  // save context
+  ctx.save();
+
+  // transform canvas
+  ctx.translate(painting.posX + (painting.naturalWidth/2), painting.posY + (painting.naturalHeight/2));
+  // rotate canvas
+  ctx.rotate(painting.rot*Math.PI/180);
+  // draw image
+  ctx.drawImage(painting, -painting.naturalWidth/2, -painting.naturalHeight/2);
+
+  // restore context
+  ctx.restore()
 }
 
 function viewPainting(posX, posY){
@@ -65,12 +79,37 @@ function viewPainting(posX, posY){
   // if merlin coords align with paiting coords in the x axis, then merlin is viewing the paiting
   if (posY <= 200){
     if (posX >= 0 && posX <= 100){ // view cow paiting
-      // ctx.drawImage(cowReaction, posX + 125, reactionHeight);
+      ctx.drawImage(cowReaction, posX + 125, reactionHeight);
     }else if (posX >= 180 && posX <= 310){ // view bear paiting
-      // ctx.drawImage(bearReaction, posX - 110, reactionHeight);
+      ctx.drawImage(bearReaction, posX - 110, reactionHeight);
     }else if (posX >= 330 && posX <= 440){ // view bunny paiting
-      // ctx.drawImage(bunnyReaction, posX - 100, reactionHeight);
+      ctx.drawImage(bunnyReaction, posX - 100, reactionHeight);
     }
+  }
+}
+
+function rotatePainting(posX, posY, clockwise){
+  let painting;
+  var rotSpeed = 5;
+
+  // determine which painting merlin is in front of
+  if(posY <= 200 ){
+    if (posX >= 0 && posX <= 100){ // rotate cow paiting
+      console.log("cow");
+      painting = cow;
+    }else if (posX >= 180 && posX <= 310){ // rotate bear paiting
+      console.log("bear");
+      painting = bear;
+    }else if (posX >= 330 && posX <= 440){ // rotate bunny paiting
+      console.log("bunny");
+      painting = bunny;
+    }
+  }
+
+  if(clockwise){
+    painting.rot += rotSpeed;
+  }else{
+    painting.rot -= rotSpeed;
   }
 }
 
@@ -98,6 +137,16 @@ function updateGallery(e, direction){
       if (merlin.posY < 270){
         merlin.posY += speed;
       }
+      break;
+    case 190: // period key pressed
+      // rotate painting clock-wise
+      rotatePainting(merlin.posX, merlin.posY, true);
+      console.log("rotate clockwise");
+      break;
+    case 188: // comma key pressed
+      // rotate painting counter clock-wise
+      rotatePainting(merlin.posX, merlin.posY, false);
+      console.log("rotate counter clock-wise");
       break;
   }
 
